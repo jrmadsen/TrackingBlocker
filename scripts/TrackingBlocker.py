7b52064e7092163a86fcb92fe6a5138424c07326
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-05-03 22:41:28 -0700

    Updated print statements to use format

--- scripts/TrackingBlocker.py
+++ scripts/TrackingBlocker.py
@@ -7,28 +7,28 @@ import filecmp
 import shutil
 from distutils.spawn import find_executable
 
-#==============================================================================#
 
+#==============================================================================#
 def ShouldModify(file1, file2):
     return not filecmp.cmp(file1, file2, shallow=False)
 
-#==============================================================================#
 
+#==============================================================================#
 def ComposeFilesToModify(data_dir, type):
     files = []
     if(type == "0" or type == "all"):
         files.append(os.path.join(data_dir, "hosts.profile.0"))
     return files
 
-#==============================================================================#
 
+#==============================================================================#
 def ComposeDataDir(prefix, data_folder):
     if(os.path.commonprefix([prefix, data_folder])):
         data_folder.replace(os.path.commonprefix([prefix, data_folder]), "")
     return os.path.join(prefix, data_folder)
 
-#==============================================================================#
 
+#==============================================================================#
 def install_cmd(args):
     exe_install_path = os.path.join(args.prefix, "bin/tracking_blocker")
 
@@ -39,13 +39,14 @@ def install_cmd(args):
     for f in args.files:
         shutil.copy2(f, os.path.join(data_dir, os.path.basename(f)))
 
-#==============================================================================#
 
+#==============================================================================#
 def get_unique(_l):
     _s = set()
     for i in _l:
         _s.add(i)
     return list(_s)
+
 
 #==============================================================================#
 # line count function
@@ -55,7 +56,7 @@ def lc(file):
     lines = f.readlines()
     f.close()
     # pop out domains from lines
-    prefixes = ["127.0.0.1"," ","www."]
+    prefixes = ["127.0.0.1", " ", "www."]
     index_delete = []
     # loop over lines
     for i in range(0, len(lines)):
@@ -80,13 +81,18 @@ def lc(file):
     # return length of lines
     return len(lines)
 
+
 #==============================================================================#
 # display number of domains blocked
 def display(args, data_dir):
     lc1 = lc(os.path.join(args.host_folder, "hosts"))
     lc2 = lc(os.path.join(data_dir, "hosts.original"))
-    print (os.path.join(args.host_folder, "hosts"), "is blocking approximately", \
-        lc1-lc2, "domains (www-prefixed and non-www-prefixed e.g., www.ad-site.com and ad-site.com)")
+    print(("{} is blocking approximately {} domains ({}, e.g. {})".format(
+        (os.path.join(args.host_folder, "hosts")),
+        lc1 - lc2,
+        "www-prefixed and non-www-prefixed",
+        "www.ad-site.com and ad-site.com")))
+
 
 #==============================================================================#
 # handle status commands
@@ -106,7 +112,7 @@ def status_cmd(args):
     elif(args.command == "display"):
         display(args, data_dir)
     else:
-        f1 = os.path.join(data_dir, "hosts.profile.%i" % (int(args.command)))
+        f1 = os.path.join(data_dir, "hosts.profile.{}".format(args.command))
         f2 = os.path.join(args.host_folder, "hosts")
         if os.path.exists(f1):
             if(ShouldModify(f1, f2)):
@@ -115,14 +121,14 @@ def status_cmd(args):
     if(args.command != "display"):
         display(args, data_dir)
 
-#==============================================================================#
 
+#==============================================================================#
 def add_cmd(args):
     data_dir = ComposeDataDir(args.prefix, args.data_folder)
     files = ComposeFilesToModify(data_dir, args.type)
 
     for fname in files:
-        print ('Adding domains to',fname)
+        print(('Adding domains to {}'.format(fname)))
         f = open(fname, 'a')
         for d in args.domain:
             d = d.lstrip("www.")
@@ -130,13 +136,13 @@ def add_cmd(args):
             f.write("127.0.0.1  www." + d + "\n")
         f.close()
 
-#==============================================================================#
 
+#==============================================================================#
 def remove_cmd(args):
     data_dir = ComposeDataDir(args.prefix, args.data_folder)
     files = ComposeFilesToModify(data_dir, args.type)
     for fname in files:
-        print ('Removing domains from',fname)
+        print(('Removing domains from {}'.format(fname)))
         # remove file
         rfname = os.path.join(data_dir, os.path.basename(fname) + ".removed")
         # load previous removes
@@ -156,12 +162,13 @@ def remove_cmd(args):
         f.close()
         # pop out domains from lines
         subtract = 0
-        prefixes = ["","www."]
+        prefixes = ["", "www."]
         for i in range(0, len(lines)):
             for d in args.domain:
                 for p in prefixes:
                     dstr = "127.0.0.1  " + p + d
-                    if(not args.all_matching and not args.load_previous_removes):
+                    if (not args.all_matching and
+                        not args.load_previous_removes):
                         dstr += " "
                     if(dstr in lines[i - subtract]):
                         line2 = lines[i - subtract].strip("127.0.0.1")
@@ -169,7 +176,7 @@ def remove_cmd(args):
                         subtract += 1
                         line2 = line2.strip('\n')
                         line2 = line2.strip()
-                        print ('Removing',line2,"...")
+                        print(('Removing {}...'.format(line2)))
                         if(not args.load_previous_removes and not p):
                             fr = open(rfname, 'a')
                             fr.write(line2 + '\n')
@@ -179,8 +186,8 @@ def remove_cmd(args):
             f.write(line)
         f.close()
 
-#==============================================================================#
 
+#==============================================================================#
 def update_cmd(args):
 
     rm_ctrl_M_path = os.path.normpath(os.path.abspath(
@@ -211,7 +218,7 @@ def update_cmd(args):
             combine_hosts_path = find_executable(args.exe_name)
 
             if(not os.path.exists(combine_hosts_path)):
-                print ('Unable to find',args.exe_name)
+                print(('Unable to find "{}"...'.format(args.exe_name)))
                 sys.exit(1)
 
     data_dir = ComposeDataDir(args.prefix, args.data_folder)
@@ -230,7 +237,7 @@ def update_cmd(args):
                 cmd.append(os.path.abspath(f))
         ret = run_command.run_command(cmd)
         if(ret > 0):
-            print ('Error running command:', join(cmd, ' '))
+            print(('Error running command: {}'.format(join(cmd, ' '))))
             sys.exit(ret)
         else:
             shutil.copy2(ofile, os.path.join(data_dir, os.path.basename(ofile)))
@@ -239,8 +246,8 @@ def update_cmd(args):
     cmd.extend([sys.argv[0], "remove", "-t", args.type, "-l"])
     run_command.run_command(cmd)
 
-#==============================================================================#
 
+#==============================================================================#
 def main(argv):
     parser = argparse.ArgumentParser()
 
@@ -255,21 +262,25 @@ def main(argv):
     subparser = parser.add_subparsers(help="sub-command help")
 
     # Install option
-    install_choice = subparser.add_parser("install", help="Install TrackingBlocker")
-    install_choice.add_argument("files", nargs='*', help="Host files to install")
+    install_choice = subparser.add_parser("install",
+        help="Install TrackingBlocker")
+    install_choice.add_argument("files", nargs='*',
+        help="Host files to install")
     install_choice.set_defaults(func=install_cmd)
 
     # Status option
-    stat_choice = subparser.add_parser("status", help="TrackingBlocker status change/display")
+    stat_choice = subparser.add_parser("status",
+        help="TrackingBlocker status change/display")
     stat_choice.add_argument("command", help="Parser command",
                              choices=['original', 'off', 'on',
                                       'profile.0', 'display'])
     stat_choice.set_defaults(func=status_cmd)
 
     # Add option
-    add_choice = subparser.add_parser("add", help="Add domain to TrackingBlocker")
+    add_choice = subparser.add_parser("add",
+        help="Add domain to TrackingBlocker")
     add_choice.add_argument("-t", "--type", help="Tracking type to add to",
-                            choices=['all','profile.0'],
+                            choices=['all', 'profile.0'],
                             default="all")
     add_choice.add_argument("domain", nargs='*', help="Domains to add")
     add_choice.set_defaults(func=add_cmd)
@@ -278,34 +289,44 @@ def main(argv):
     remove_choice = subparser.add_parser("remove", help="Remove domain from "
         + "TrackingBlocker (removed domains are added to "
         + "{PREFIX}/etc/TrackingBlocker/hosts.{type}.removed)")
-    remove_choice.add_argument("-t", "--type", help="Tracking type to remove from",
-                               choices=['all','profile.0'],
-                               default="all")
+    remove_choice.add_argument("-t", "--type",
+        help="Tracking type to remove from",
+        choices=['all', 'profile.0'],
+        default="all")
     remove_choice.add_argument("-a", "--all-matching", action="store_true",
-                               help="Remove all domains starting with domains listed")
-    remove_choice.add_argument("-l", "--load-previous-removes", action="store_true",
-                               help="Reremove previous stored in hosts.{type}.removed")
+        help="Remove all domains starting with domains listed")
+    remove_choice.add_argument("-l", "--load-previous-removes",
+        action="store_true",
+        help="Reremove previous stored in hosts.{type}.removed")
     remove_choice.add_argument("domain", nargs='*', help="Domains to remove")
     remove_choice.set_defaults(func=remove_cmd)
 
     update_choice = subparser.add_parser("update", help="Update domains")
-    update_choice.add_argument("-f", "--files", help="Files to include in update",
-                               nargs='*')
-    update_choice.add_argument("-u", "--update", help="Update with WINHOSTS",
-                               action="store_true")
-    update_choice.add_argument("-o", "--output-dir", help="temporary folder to hold outputs",
-                               default="/tmp/4wejsdfasdff")
-    update_choice.add_argument("-d","--default", help="default HOST file",
-                               required=True)
-    update_choice.add_argument("-t", "--type", help="Tracking type to update",
-                               choices=['all','profile.0'],
-                               default="profile.0")
-    update_choice.add_argument("--exe-path", help="Path to combine_hosts exe",
-                               default="../libexec")
-    update_choice.add_argument("--exe-name", help="Alternative name to \"combine_hosts\"",
-                               default="combine_hosts")
-    update_choice.add_argument("--rm-ctrl-M", help="Script to remove ^M characters",
-                               default="pyscripts")
+    update_choice.add_argument("-f", "--files",
+        help="Files to include in update",
+        nargs='*')
+    update_choice.add_argument("-u", "--update",
+        help="Update with WINHOSTS",
+        action="store_true")
+    update_choice.add_argument("-o", "--output-dir",
+        help="temporary folder to hold outputs",
+        default="/tmp/4wejsdfasdff")
+    update_choice.add_argument("-d", "--default",
+        help="default HOST file",
+        required=True)
+    update_choice.add_argument("-t", "--type",
+        help="Tracking type to update",
+        choices=['all', 'profile.0'],
+        default="profile.0")
+    update_choice.add_argument("--exe-path",
+        help="Path to combine_hosts exe",
+        default="../libexec")
+    update_choice.add_argument("--exe-name",
+        help="Alternative name to \"combine_hosts\"",
+        default="combine_hosts")
+    update_choice.add_argument("--rm-ctrl-M",
+        help="Script to remove ^M characters",
+        default="pyscripts")
     update_choice.set_defaults(func=update_cmd)
 
     #if(not subprocess.call(join(["sudo", "-n", "echo", "-n"], ' '))):
@@ -322,11 +343,10 @@ def main(argv):
     args.func(args)
 
 
-
 #==============================================================================#
-
 if __name__ == "__main__":
-    script_path = os.path.abspath(os.path.dirname(os.path.realpath(sys.argv[0])))
+    this_path = os.path.realpath(sys.argv[0])
+    script_path = os.path.abspath(os.path.dirname(this_path))
     sys.path.append(os.path.join(script_path, "pypath"))
     from run_command import run_command, join
     main(sys.argv[1:])
